996a7ab51cc8ba0998aad684ac4cbdc973f4ed4f
fawad khan <fovadkhan@outlook.com>
2024-09-21 23:43:15 +0200

    chore: add icons

--- src/components/Table/TableHeader.tsx
+++ src/components/Table/TableHeader.tsx
@@ -1,10 +1,28 @@
 import React from "react";
 import { Column } from "../../types";
+import classNames from "./Table.module.css";
 
-export const TableHeader: React.FC<{ columns: Column[] }> = ({ columns }) => (
+import { ReactComponent as AscIcon } from '../../assets/order-ascending.svg';
+import { ReactComponent as DscIcon } from '../../assets/order-descending.svg';
+
+export const TableHeader: React.FC<{ columns: Column[] }> = ({ 
+  columns,
+
+ }) => (
     <tr>
       {columns.map(({ id, title }) => (
-        <th key={id}>{title}</th>
+        <th key={id}>
+          {title}
+          { title && (
+          <>
+            {title === 'ascending' ? (
+              <AscIcon className={classNames[`sort-icon`]} />
+            ) : (
+              <DscIcon className={classNames[`sort-icon`]} />
+            )}
+          </>
+        )}
+        </th>
       ))}
     </tr>
   );
