65ecbc33b49f85611ed30932ea312d31063419b5
fawad khan <fovadkhan@outlook.com>
2024-09-23 20:50:30 +0200

    chore: refactor title styles

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -9,7 +9,7 @@ import { TableBody } from './TableBody';
 const TableTitle: React.FC = () => (
   <>
     <h3>Sortable and Filterable Table</h3>
-    <p style={{ fontSize: "0.9em", color: "#6c757d" }}>
+    <p style={{ fontSize: "0.9em", color: "#6c757d" }}> {/* // TODO : remove inline styles from here  */}
       Click on the column titles to sort the table.
     </p>
   </>
